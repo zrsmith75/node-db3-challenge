82ca306fac78c5cb3cb76d4a1403fdd6cc32df1d
Zac Smith <zrs3141592@gmail.com>
2019-08-29 18:37:52 -0600

    create add, works correctly

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,7 +3,8 @@ const db = require("../data/dbConfig.js");
 module.exports = {
   find,
   findById,
-  findSteps
+  findSteps,
+  add
 };
 
 function find() {
@@ -14,6 +15,21 @@ function findById(id) {
   return db("schemes").where({ id });
 }
 
+// function findSteps(id) {
+//   return db("steps").where({ scheme_id: id });
+// }
+
 function findSteps(id) {
-  return db("steps").where({ scheme_id: id });
+  return db("schemes")
+    .innerJoin("steps", "schemes.id", "steps.scheme_id")
+    .where({ scheme_id: id })
+    .select("schemes.scheme_name", "steps.step_number", "steps.instructions");
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme, "id")
+    .then(id => {
+      return findById(...id);
+    });
 }
