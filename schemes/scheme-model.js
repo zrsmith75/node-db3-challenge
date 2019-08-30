6bc5f299ad5180fb60cc9650931e771fbcf7e328
Zac Smith <zrs3141592@gmail.com>
2019-08-29 18:09:11 -0600

    findById and findBySteps created, working correctly

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,15 +1,19 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-  find
-  // findById,
-  // findSteps
+  find,
+  findById,
+  findSteps
 };
 
 function find() {
   return db("schemes");
 }
 
-// function findById() {
-//   return db('schemes')
-// }
+function findById(id) {
+  return db("schemes").where({ id });
+}
+
+function findSteps(id) {
+  return db("steps").where({ scheme_id: id });
+}
